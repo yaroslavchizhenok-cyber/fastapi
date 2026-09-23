842829f4f8cc39763b2627150cc3cf21e6f6f630
Yaroslav <yaroslavchizhenok@gmail.com>
2026-09-23 13:37:43 +0500

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "title": "FastAPI is Awesome",
+        "content": "This framework is really easy to use and super fast.",
+        "date_posted": "April 20, 2025",
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development, and FastAPI make",
+        "date_posted": "April 21, 2025",
+    },
+]
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message": "Hello, World!"}
+    return f"<h1>Hello, World! {posts[0]['title']}</h1>"
+@app.get("/api/posts")
+def get_posts():
+    return {"posts": posts}
